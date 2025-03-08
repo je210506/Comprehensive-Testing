3a7db0aab8ccc225384eb94311af4d3d25edc799
je210506 <je210506@ucf.edu>
2025-03-07 23:24:27 -0500

    save

--- cypress/e2e/quiz.cy.js
+++ cypress/e2e/quiz.cy.js
@@ -1,43 +1,59 @@
-// import { mockState } from '../support/utils/helpers';
-// import { Responses } from '../support/types';
+import { mount } from "cypress/react";
+import Quiz from "../../client/src/components/Quiz";
 
 describe('Quiz Component', () => {
-  //context('Game Setup', () => {
+
     beforeEach(() => {
-      // Stub the API request for starting the game      
-      cy.visit('/');
-    });
+      cy.intercept({
+        method: "GET",
+        url: "/api/questions/random"
+      },
+      {
+        fixture: "questions.json",
+        statusCode: 200,
+      }
+    ).as("getRandomQuestion") 
+    
+    })
 
     it('should display the start button when the page loads', () => {
       // Wait for the API call to complete
-      cy.get('button#Start').should('be.visible').and('contain', 'Start Quiz');
+      mount(<Quiz />);
+      cy.get('button').should('be.visible').contains('Start Quiz');
       
     });
 
     it('should start the quiz when the button is clicked', () => {
-
-      cy.get('button#Start').click();
-      cy.get('.questions').should('exist');
-      cy.get('.answers').should('have.length.greaterThan', 0);
+      mount(<Quiz />);
+      cy.get('button').contains('Start Quiz').click();
+      cy.get('.card').should('be.visible');
     });
 
-    it('should allow the user to answer the question', () => {
-
-      cy.get('button#start').click();
-      cy.get('.answers button').first().click();
-      cy.get('.questions').should('exist');
-      cy.get('.answers').should('have.length.greaterThan', 0);
+    it('should allow the user to answer a question', () => {
+      mount(<Quiz />)
+      cy.get('button').click();
+      cy.get('[data-test="answer-option"]').first().click(); // Click the first answer option
+      cy.get('[data-test="answer-option"]').should('have.length.greaterThan', 0); // Ensure answer options exist
     });
 
-    it('should show the result after answering all questions', () => {
-      // Start the quiz
-      cy.get('button#start').click();
-      // Answer all questions 
-      cy.get('.answers button').each(($btn) => {
-        cy.wrap($btn).click();
-      });
-      // Verify the final result screen shows after all questions have been answered
-      cy.get('.result').should('be.visible');
+    it("should complete the quiz when the last question gets answered", () => {
+      cy.mount(<Quiz />);
+      cy.get("button").contains("Start Quiz").click();
+      for (let i=0; i<20; i++) {
+        cy.get("button").contains("1").click()
+      }
+      cy.get("h2").contains("Quiz Completed").should("exist");
     });
-  
+
+    it("should Restart the quiz", () => {
+      cy.mount(<Quiz />);
+      cy.get("button").contains("Start Quiz").click();
+      for (let i=0; i<20; i++) {
+        cy.get("button").contains("1").click()
+      }
+      cy.get("button").click();
+      cy.get(".card").should("be.visible");
+    })
+
   });
+  
