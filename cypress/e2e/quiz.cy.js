6aa8be00a70250db22a696edae5b8f0dc2287baf
je210506 <je210506@ucf.edu>
2025-03-10 10:14:05 -0400

    removed mount

--- cypress/e2e/quiz.cy.js
+++ cypress/e2e/quiz.cy.js
@@ -1,5 +1,5 @@
-import { mount } from "cypress/react";
-import Quiz from "../../client/src/components/Quiz";
+// import { mount } from "cypress/react";
+// import Quiz from "../../client/src/components/Quiz";
 
 describe('Quiz Component', () => {
 
@@ -14,30 +14,27 @@ describe('Quiz Component', () => {
       }
     ).as("getRandomQuestion") 
     
+    cy.visit('/')
     })
 
     it('should display the start button when the page loads', () => {
       // Wait for the API call to complete
-      mount(<Quiz />);
       cy.get('button').should('be.visible').contains('Start Quiz');
       
     });
 
     it('should start the quiz when the button is clicked', () => {
-      mount(<Quiz />);
       cy.get('button').contains('Start Quiz').click();
       cy.get('.card').should('be.visible');
     });
 
     it('should allow the user to answer a question', () => {
-      mount(<Quiz />)
       cy.get('button').click();
       cy.get('[data-test="answer-option"]').first().click(); // Click the first answer option
       cy.get('[data-test="answer-option"]').should('have.length.greaterThan', 0); // Ensure answer options exist
     });
 
     it("should complete the quiz when the last question gets answered", () => {
-      cy.mount(<Quiz />);
       cy.get("button").contains("Start Quiz").click();
       for (let i=0; i<20; i++) {
         cy.get("button").contains("1").click()
@@ -46,7 +43,6 @@ describe('Quiz Component', () => {
     });
 
     it("should Restart the quiz", () => {
-      cy.mount(<Quiz />);
       cy.get("button").contains("Start Quiz").click();
       for (let i=0; i<20; i++) {
         cy.get("button").contains("1").click()
