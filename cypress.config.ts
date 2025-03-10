dc58f8154068dad28c0501754b35b886f8cafa26
je210506 <je210506@ucf.edu>
2025-03-10 14:36:24 -0400

    save

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    baseUrl: 'http://localhost:3001',
+    setupNodeEvents(_on, _config) {
       // implement node event listeners here
     },
   },
