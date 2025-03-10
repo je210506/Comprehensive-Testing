cd5126fc948e5c2a71c1c6bbbb06edb3be4589d6
je210506 <je210506@ucf.edu>
2025-03-10 16:55:04 -0400

    e2e tests work

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,9 +2,16 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3001',
+    baseUrl: "http://localhost:3001",
     setupNodeEvents(_on, _config) {
       // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
     },
   },
 });
--- cypress/e2e/quiz.cy.js
+++ cypress/e2e/quiz.cy.js
@@ -3,41 +3,33 @@
 
 describe('Quiz Component', () => {
 
-    beforeEach(() => {
-      cy.intercept({
-        method: "GET",
-        url: "/api/questions/random"
-      },
-      {
-        fixture: "questions.json",
-        statusCode: 200,
-      }
-    ).as("getRandomQuestion") 
-    
-    cy.visit('/');
-    cy.wait("@getRandomQuestion");
-    });
+  beforeEach(() => {
+    cy.visit('http://localhost:3001/');
+  });
+
 
     it('should display the start button when the page loads', () => {
       // Wait for the API call to complete
+      cy.visit('http://localhost:3001/');
       cy.get('button').should('be.visible').contains('Start Quiz');
       
     });
 
     it('should start the quiz when the button is clicked', () => {
+      
       cy.get('button').contains('Start Quiz').click();
       cy.get('.card').should('be.visible');
     });
 
+    //math.random
     it('should allow the user to answer a question', () => {
-      cy.get('button').click();
-      cy.get('[data-test="answer-option"]').first().click(); // Click the first answer option
-      cy.get('[data-test="answer-option"]').should('have.length.greaterThan', 0); // Ensure answer options exist
+      // cy.get('.btn').eq(Math.floor(Math.random() *4)).click();
+      cy.get(':nth-child(1) > .btn').first().click(); // Click the first answer option
     });
 
     it("should complete the quiz when the last question gets answered", () => {
-      cy.get("button").contains("Start Quiz").click();
-      for (let i=0; i<20; i++) {
+      cy.get('.btn').contains("Start Quiz").click();
+      for (let i=0; i<10; i++) {
         cy.get("button").contains("1").click()
       }
       cy.get("h2").contains("Quiz Completed").should("exist");
@@ -45,10 +37,10 @@ describe('Quiz Component', () => {
 
     it("should Restart the quiz", () => {
       cy.get("button").contains("Start Quiz").click();
-      for (let i=0; i<20; i++) {
-        cy.get("button").contains("1").click().wait()
+      for (let i=0; i<10; i++) {
+        cy.get("button").contains("1").click()
       }
-      cy.get("button").click();
+      cy.get(".btn").click();
       cy.get(".card").should("be.visible");
     })
 
